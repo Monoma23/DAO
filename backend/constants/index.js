c1650939147398e89cde9736b12a0e5e7139ca20
Monoma23 <ayoubshow5@gmail.com>
2022-08-26 14:44:38 +0200

    firds commit

---
+++ backend/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x6c4CBBD86d199968579ff26Ac567FeB3869Cf952";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
